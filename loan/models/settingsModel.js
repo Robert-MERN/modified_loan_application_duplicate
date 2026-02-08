a45a2382227927cc2b5e0f18e3868861956f7105
Muneeb Ahmed <muneebsafeer@gmail.com>
2026-02-08 19:19:11 +0500

    done

--- loan/models/settingsModel.js
+++ loan/models/settingsModel.js
@@ -22,16 +22,8 @@ const settingSchema = new Schema(
         },
     },
     { timestamps: true });
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 62934d48deee077498187ff43c56fe045344648a
 const Db = connection.useDb("LoanDuplicate");
 const Settings = Db.models.Settings || Db.model('Settings', settingSchema);
 export default Settings
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 62934d48deee077498187ff43c56fe045344648a
